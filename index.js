4d7b0ba8c0dbb212d7800226948bd62b3d294743
Arthur Borges <arthurborges.dev@gmail.com>
2023-05-30 16:52:14 -0300

    Implemented initial version of load CSV and conversion to JSON

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const multer = require('multer');
+const csv = require('csvtojson');
+
 const app = express();
+
+const upload = multer({ dest: 'uploads/' });
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.post('/api/files', upload.single('csv-file'), (req, res) => {
+  if (req.method !== 'POST') {
+    res.status(405).send(`${req.method} method Not Allowed`);
+  }
+
+  csv().fromFile(req.file.path).then((jsonObj) => {
+    console.log(jsonObj);
+  });
+
   res.send('Hello World!')
 });
 
